4a76cf9716ad4e335202a360138943f5f38967bd
flydahli <44510868+flydahli@users.noreply.github.com>
2024-10-13 10:58:12 +0100

    Add source code from Fiddle

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
-/// <reference types="@fastly/js-compute" />
+import { allowDynamicBackends } from "fastly:experimental";
+import { Backend } from "fastly:backend";
 
-addEventListener("fetch", (event) => event.respondWith(handleRequest(event)));
+allowDynamicBackends(true);
 
-async function handleRequest(event) {
-  return new Response("OK", { status: 200 });
-}
+addEventListener("fetch", event => {
+  const backend = new Backend({
+    name: 'my_backend',
+    target: 'http-me.glitch.me',
+    hostOverride: "http-me.glitch.me",
+    connectTimeout: 1000,
+    firstByteTimeout: 15000,
+    betweenBytesTimeout: 10000,
+    useSSL: true,
+    sslMinVersion: 1.3,
+    sslMaxVersion: 1.3,
+  });
+  return fetch(event.request, { backend });
+});
